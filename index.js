b15f9bc534a425f0f46ce30e3df5174458d6b163
Robert W <robwash@gmail.com>
2017-09-16 16:46:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var katzDeli = []
 function takeANumber(katzDeliLine, name){
 
   katzDeliLine.push(name)
-  
+
     return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
@@ -16,13 +16,7 @@ function nowServing(katzDeliLine){
 
     deliLine.push(katzDeliLine)
 
-      if(deliLine === emptyArray){
-        return "There is nobody waiting to be served!"
-      }
-      else if(deliLine > 0){
-        //deliLine.pop()
-        return `Currently serving ${console.log(deliLine[0])}`
-      }
+      
 }
 
 function currentLine(katzDeliLine){
